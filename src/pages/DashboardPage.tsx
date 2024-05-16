2976102f5edf06617a4698d8e91eecd3d1c91f47
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2024-05-16 21:31:47 +0800

    added profile panel

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -151,8 +151,32 @@ const DashboardPage = () => {
         <div className="flex flex-row h-full w-1/3">
           <div className="h-full w-full">
             {/* Profile */}
-            <div className="flex flex-col bg-white shadow-md h-full w-full rounded-xl p-4">
-              <div>Lagay mo profile pic nila dito sheesh</div>
+            <div className="flex flex-col space-y-3 bg-white shadow-md h-full w-full rounded-xl p-6 items-center">
+              <div className="flex flex-row space-x-3 w-full border-b-[1px] rounded-sm border-gray-300 pb-4">
+                <div className="avatar online w-1/4">
+                  <div className="w-24 rounded-full">
+                    <img
+                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col w-3/4">
+                  <h3 className="font-semibold text-lg text-black">
+                    {user.user_.firstName} {user.user_.lastName}
+                  </h3>
+                  <h3 className="text-caribbean-700 font-semibold">Novice</h3>
+
+                  <h3 className="text-black font-semibold mt-2">
+                    <span className="bg-gray-400 p-1 px-2 rounded-xl text-white shadow-md">
+                      Total Points: 200
+                    </span>
+                  </h3>
+                </div>
+              </div>
+              <div>Badges</div>
+              <div>Calendar</div>
+              <div>To do</div>
             </div>
           </div>
         </div>
